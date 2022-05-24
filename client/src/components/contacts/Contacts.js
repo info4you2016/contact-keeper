7d4296b39f88d7f1a2de2bfef7899d7309206a9d
Mohamed <info4you2013@gmail.com>
2022-05-24 23:13:57 +0100

    next

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,18 +1,32 @@
-import React, {Fragment, useContext} from 'react'
-import ContactItem from './ContactItem';
-import ContactContext from '../../context/contacts/ContactContext';
+import React, { Fragment, useContext } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import ContactItem from "./ContactItem";
+import ContactContext from "../../context/contacts/ContactContext";
 
 const Contacts = () => {
-    const contactContext = useContext(ContactContext);
+	const contactContext = useContext(ContactContext);
 
-    const {contacts} = contactContext;
-  return (
-    <Fragment>
-        {contacts.map(contact => (
-            <ContactItem contact={contact} key={contact.id} />
-        ))}
-    </Fragment>
-  )
-}
+	const { contacts, filtered } = contactContext;
+	if (contacts.length === 0) {
+		return <h4>Please add a contact</h4>;
+	}
+	return (
+		<Fragment>
+			<TransitionGroup>
+				{filtered !== null
+					? filtered.map((contact) => (
+							<CSSTransition key={contact.id} timeout={500} classNames="item">
+								<ContactItem contact={contact} />
+							</CSSTransition>
+					  ))
+					: contacts.map((contact) => (
+							<CSSTransition key={contact.id} timeout={500} classNames="item">
+								<ContactItem contact={contact} />
+							</CSSTransition>
+					  ))}
+			</TransitionGroup>
+		</Fragment>
+	);
+};
 
-export default Contacts
+export default Contacts;
--- client/src/context/contacts/ContactState.js
+++ client/src/context/contacts/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import uuid from "uuid";
+import { v4 as uuidv4 } from 'uuid';
 import ContactContext from "./ContactContext";
 import ContactReducer from "./ContactReducer";
 import {
@@ -37,14 +37,54 @@ const ContactState = (props) => {
 				type: "professional",
 			},
 		],
+		current: null,
+		filtered: null
 	};
 
     const [state, dispatch] = useReducer(ContactReducer, initialState);
 
+	const addContact = contact => {
+		contact.id = uuidv4();
+		dispatch({ type: ADD_CONTACT, payload: contact});
+	};
+
+	const deleteContact = id => {
+		dispatch({ type: DELETE_CONTACT, payload: id});
+	};
+
+	const setCurrent = contact => {
+		dispatch({ type: SET_CURRENT, payload: contact});
+	};
+
+	const updateContact = contact => {
+		dispatch({ type: UPDATE_CONTACT, payload: contact});
+	};
+
+	const clearCurrent = () => {
+		dispatch({ type: CLEAR_CURRENT});
+	};
+
+	const filterContacts = text => {
+		dispatch({ type: FILTER_CONTACTS, payload: text});
+	}
+
+	const clearFilter = () => {
+		dispatch({ type: CLEAR_FILTER});
+	};
+
     return (
         <ContactContext.Provider
         value={{
-            contacts: state.contacts
+            contacts: state.contacts,
+			current: state.current,
+			filtered: state.filtered,
+			addContact,
+			deleteContact,
+			setCurrent,
+			clearCurrent,
+			updateContact,
+			filterContacts,
+			clearFilter
         }}>
             { props.children }
         </ContactContext.Provider>
